6bd64ebc75950e56efa36a7862cfe37401d41493
naimaloom1 <69838717+naimaloom1@users.noreply.github.com>
2020-11-01 13:33:34 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -9,7 +9,148 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "resourcegroup-test-tbd-3"
-  location = "westeurope"
+# Create a resource group
+resource "azurerm_resource_group" "Prod" {
+  name     = "myresourcegroup01"
+  location = "West Europe"
 }
+resource "azurerm_public_ip" "Prodlb" {
+  name                = "PublicIPForLB"
+  location            = "West Europe"
+  resource_group_name = "${azurerm_resource_group.Prod.name}"
+  allocation_method   = "Static"
+}
+resource "azurerm_lb" "Prod" {
+  name                = "TestLoadBalancer"
+  location            = "West Europe"
+  resource_group_name = "${azurerm_resource_group.Prod.name}"
+
+  frontend_ip_configuration {
+    name                 = "LBPip"
+    public_ip_address_id = "${azurerm_public_ip.Prodlb.id}"
+  }
+}
+resource "azurerm_lb_backend_address_pool" "Prod" {
+  resource_group_name = "${azurerm_resource_group.Prod.name}"
+  loadbalancer_id     = "${azurerm_lb.Prod.id}"
+  name                = "BackEndAddressPool"
+}
+resource "azurerm_lb_rule" "Prod" {
+  resource_group_name            = "${azurerm_resource_group.Prod.name}"
+  loadbalancer_id                = "${azurerm_lb.Prod.id}"
+  name                           = "LBRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 3389
+  backend_port                   = 3389
+  frontend_ip_configuration_name = "LBPip"
+}
+resource "azurerm_public_ip" "Prod" {  //Here defined the public IP
+  name                         = "VMpublicIP"  
+  location                     = "${azurerm_resource_group.Prod.location}"  
+  resource_group_name          = "${azurerm_resource_group.Prod.name}"  
+  allocation_method            = "Static"  
+  idle_timeout_in_minutes      = 30  
+  domain_name_label            = "myvm1"
+}
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "Prod" {
+  name                = "myvnet"
+  resource_group_name = "${azurerm_resource_group.Prod.name}"
+  location            = "${azurerm_resource_group.Prod.location}"
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "Prod" {
+  name                 = "mysubnet"
+  resource_group_name  = "${azurerm_resource_group.Prod.name}"
+  virtual_network_name = "${azurerm_virtual_network.Prod.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+resource "azurerm_storage_account" "Prod" {
+  name                     = "wedstracc"
+  resource_group_name      = "${azurerm_resource_group.Prod.name}"
+  location                 = "${azurerm_resource_group.Prod.location}"
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  enable_https_traffic_only= "true"
+}
+resource "azurerm_storage_account_network_rules" "Prod" {
+  resource_group_name  = "${azurerm_resource_group.Prod.name}"
+  storage_account_name = "${azurerm_storage_account.Prod.name}"
+
+  default_action             = "Allow"
+  ip_rules                   = []
+  virtual_network_subnet_ids = []
+  bypass                     = ["AzureServices"]
+}
+resource "azurerm_storage_container" "Prod" {
+  name                  = "content"
+  storage_account_name  = "${azurerm_storage_account.Prod.name}"
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "Prod" {
+  name                   = "mysite"
+  storage_account_name   = "${azurerm_storage_account.Prod.name}"
+  storage_container_name = "${azurerm_storage_container.Prod.name}"
+  type                   = "Block"
+  source                 = "./SITE.jpg"
+}
+resource "azurerm_network_security_group" "Prod" {
+  name                = "mydemo-nsg"
+  location            = "${azurerm_resource_group.Prod.location}"
+  resource_group_name = "${azurerm_resource_group.Prod.name}"
+
+security_rule {   //Here opened remote desktop port
+    name                       = "RDP"  
+    priority                   = 110  
+    direction                  = "Inbound"  
+    access                     = "Allow" 
+    protocol                   = "Tcp"  
+    source_port_range          = "*"  
+    destination_port_range     = "3389"  
+    source_address_prefix      = "*"  
+    destination_address_prefix = "*"  
+  }
+}
+
+resource "azurerm_network_interface" "Prod" {
+  name                = "vm1nic"
+  location            = "${azurerm_resource_group.Prod.location}"
+  resource_group_name = "${azurerm_resource_group.Prod.name}"
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = "${azurerm_subnet.Prod.id}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.Prod.id}"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "Prod" {
+  network_interface_id      = "${azurerm_network_interface.Prod.id}"
+  network_security_group_id = "${azurerm_network_security_group.Prod.id}"
+}
+resource "azurerm_windows_virtual_machine" "Prod" {
+  name                = "myvm1"
+  resource_group_name = "${azurerm_resource_group.Prod.name}"
+  location            = "${azurerm_resource_group.Prod.location}"
+  size                = "Standard_DS2_v2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.Prod.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
